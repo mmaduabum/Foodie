8a0c32ff06fda89ee5f80f13a56411971709cccf
root <root@ip-172-31-44-68.us-west-2.compute.internal>
2016-05-05 20:30:41 +0000

    idk

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ AUTH_TOKEN = "e5ecf02cc2def042420962c7bb4345df"
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
  
 client.messages.create(
-    to="+19739705321", 
+    to="+16464630213", 
     from_="+16508351609", 
-    body="Hey Mike! Good luck on the bar exam!", 
+    body="Trump2016!", 
 )
