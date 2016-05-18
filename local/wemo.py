38a6ac9ff82c378e9b0958dd080735c90d3507a9
Michael Maduabum <michaelmaduabum@Michaels-MacBook-Pro-18.local>
2016-05-17 22:36:42 -0700

    wemo monitoring

--- local/wemo.py
+++ local/wemo.py
@@ -1,4 +1,9 @@
 from ouimeaux.environment import Environment
+import time
+import httplib
+
+ON = "on"
+OFF = "off"
 
 def on_switch(switch):
     print "Switch found!", switch.name
@@ -12,4 +17,25 @@ env.start()
 #env.list_switches()
 
 switch = env.get_switch('ymcmb')
-print switch.current_power
+
+
+def notify_AWS(state):
+	conn = httplib.HTTPConnection("52.39.99.178")
+	msg = "RAMYour device is now " + state
+	conn.request("POST", "", msg)
+
+
+last_state = "off"
+
+while True:
+	power = switch.current_power
+	print "power is " + str(power)
+	if power < 10 and last_state == "on":
+		notify_AWS(OFF)
+		last_state = "off"
+	elif power > 2000 and last_state == "off":
+		notify_AWS(ON)
+		last_state = "on"
+	time.sleep(2)
+
+
