0f88026c3cb347cd51ad5307cf74ad0781705071
root <root@ip-172-31-44-68.us-west-2.compute.internal>
2016-05-30 22:33:36 +0000

    subscription stuff

--- AWS/listen.py
+++ AWS/listen.py
@@ -8,7 +8,9 @@ FROM = "'From'"
 MSG = "'Body'"
 SPOOKY_INDEX = 4
 US = "+16508351609"
-
+BEAKS = "+16464630213"
+subscriber = BEAKS
+subscribed = False
 
 def crash():
     sys.exit()
@@ -30,31 +32,62 @@ def reply_to_sender(dic):
     messager.send_message(sender, response)
 
 
+def handle_message(dic):
+    global subscriber
+    global subscribed
+    msg = dic[MSG].replace("+", " ").lower()
+    sender = dic[FROM][SPOOKY_INDEX:]
+    if "subscribe" in msg and "unsubscribe" not in msg:
+        if subscribed:
+            if sender == subscriber: messager.send_message(sender, "you are already subscribed")
+            else: messager.send_message(sender,"Sorry, this device is currently locked")
+        else:
+            subscribed = True
+            subscriber = sender
+            messager.send_message(sender, "You are now subscribed to your device! To unsubscribe, reply 'unsubscribe'")
+
+    elif "unsubscribe" in msg:
+        if subscribed:
+            subscribed = False
+            subscriber = BEAKS
+            messager.send_message(sender, "You have unsubscribed. To resubscribe, reply 'subscribe'")
+        else:
+            messager.send_message(sender, "You are not subscribed to a device. To subscribe, reply 'subscribe'")
+
+    else:
+        messager.send_message(sender, "Hey there! Unfortunately, we don't recognize your request. To subscribe to a device, reply 'subscribe'")
+
 # gets the text message content from the HTTP request
-def handle_Twilio_response(incoming_message):
+def parse_Twilio_response(incoming_message):
     #couldnt find a lib to parse the http data. This function makes it into a dictionary
     list_of_fields = incoming_message.replace('&', "','").replace("=", "':'").split(',')
     dic = {}
     for field in list_of_fields:
         p = field.split(':')
         dic[p[0]] = p[1]
-    reply_to_sender(dic)
+    handle_message(dic)
 
-#listen on port 80 in loop. probably a better way to do this
-errors = 0
-while True:
-    try:
+def ram():
+    #listen on port 80 in loop. probably a better way to do this
+    while True:
         incoming_message = str(sh.nc('-l', '-w', '1', '80'))
         if "Twilio" not in incoming_message:
             msg_start = incoming_message.index("RAM") + 3
             response = incoming_message[msg_start:]
-            messager.send_message(msg=response)
+            messager.send_message(subscriber, msg=response)
         else:
-            handle_Twilio_response(incoming_message)
+            parse_Twilio_response(incoming_message)
+
+
+def main():
+    subscribed = False
+    subscriber = BEAKS
+    try:
+        ram()
     except Exception as e:
-        #if errors > 100: crash()
         f = open('crashes.txt', 'a+')
         f.write(str(e))
         f.write('\n')
-        errors += 1
 
+
+main()
