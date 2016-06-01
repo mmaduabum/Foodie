9a16e1db8e3148767f7bf6523d86f06a34dc671f
Michael Maduabum <michaelmaduabum@DN51utdp.SUNet>
2016-05-31 23:37:02 -0700

    threshold stuff

--- local/wemo.py
+++ local/wemo.py
@@ -25,15 +25,26 @@ def notify_AWS(state):
 	conn.request("POST", "", msg)
 
 
-last_power = 0
+def powering_off(history):
+	old, med, new = history
+	return old > med and med > new
+
+def powering_on(history):
+	old, med, new = history
+	return old < med and med < new
+
+
+last_powers = [0, 0, 0]
 
 while True:
 	power = switch.current_power
+	l.pop(0)
+	l.append(power)
 	print "power is " + str(power)
-	if power < 4500 and last_power > 4500:
+	if power < 4500 and last_power powering_off(last_powers):
 		notify_AWS(OFF)
 		last_state = "off"
-	elif power > 100 and last_power < 100:
+	elif powering_on(last_powers):
 		notify_AWS(ON)
 		last_state = "on"
 	time.sleep(2)
