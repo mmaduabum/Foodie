e8d0e75a3585ed9c83ffa7b8e09675eae66fa4db
root <root@ip-172-31-44-68.us-west-2.compute.internal>
2016-06-04 06:11:12 +0000

    moved twilio paring to twilio parsing file

--- AWS/twilio_parser.py
+++ AWS/twilio_parser.py
@@ -1,1 +1,25 @@
-#new file
+import sh
+from twilio.rest import TwilioRestClient
+import sys
+import messager
+import sqlite3
+
+sqlite_file = 'monica.db'
+FROM = "'From'"
+MSG = "'Body'"
+SPOOKY_INDEX = 4
+US = "+16508351609"
+BEAKS = "+16464630213"
+subscriber = BEAKS
+subscribed = False
+port = "80"
+
+
+def twilio_to_dic(incoming_message):
+    #couldnt find a lib to parse the http data. This function makes it into a dictionary
+    list_of_fields = incoming_message.replace('&', "','").replace("=", "':'").split(',')
+    dic = {}
+    for field in list_of_fields:
+        p = field.split(':')
+        dic[p[0]] = p[1]
+    return dic
