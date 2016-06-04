2ab044a0ac0dac009d020fc4f800bac0a5db815e
Michael Maduabum <michaelmaduabum@DN51usrg.SUNet>
2016-06-03 23:21:42 -0700

    added some skeletons

--- AWS/switch_processor.py
+++ AWS/switch_processor.py
@@ -1,1 +1,19 @@
-#new file
+import sh
+from twilio.rest import TwilioRestClient
+import sys
+import messager
+import sqlite3
+import twilio_parser as parser
+
+
+
+"""update the database on state changes for any switches that
+	have changed state or any user's whose subscribtion has ended"""
+def update_state():
+	pass
+
+
+"""Listen on port 3657 for incoming status from the Pi"""
+def accept_command():
+	pass
+
--- AWS/user_processor.py
+++ AWS/user_processor.py
@@ -1,1 +1,21 @@
-#new file
+import sh
+from twilio.rest import TwilioRestClient
+import sys
+import messager
+import sqlite3
+import twilio_parser as parser
+
+"""execute the user's command"""
+def process_command():
+	pass
+
+
+"""Notify the user on the status of the command"""
+def respond_to_user():
+	pass
+
+
+"""Listen on port 80 for incoming user command"""
+def accept_command():
+	pass
+
