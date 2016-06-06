3f3796d7fd168ae9d9ef54d9c5ae1bc1deccd37e
Michael Maduabum <michaelmaduabum@DN51us6e.SUNet>
2016-06-05 23:34:09 -0700

    untested wemo stuff

--- local/wemo.py
+++ local/wemo.py
@@ -1,11 +1,21 @@
 from ouimeaux.environment import Environment
 import time
 import httplib
+import json
+import sys
 
-ON = "on"
-OFF = "off"
-THRESH = 400
-
+ON = 1
+OFF = 0
+THRESH = "threshold"
+YMCMB_THRESH = 4500
+PADDY_THRESH = 400
+UB_PORT_NUMBER = 3758
+NAME = "name"
+SWITCH = "switch"
+STATE = "state"
+YMCMB_ID = 1
+PADDY_ID = 2
+ID = "ID"
 
 def on_switch(switch):
     print "Switch found!", switch.name
@@ -13,20 +23,14 @@ def on_switch(switch):
 def on_motion(motion):
     print "Motion found!", motion.name
 
-env = Environment(on_switch, on_motion)
-env.start()
-
-#env.list_switches()
-
-switch = env.get_switch('ymcmb')
 
 
-def notify_AWS(state):
-	conn = httplib.HTTPConnection("52.39.99.178")
-	if state == OFF:
-		msg = "RAMYour device is now " + state + ". To unfollow, reply 'unfollow'" 
-	else:
-		msg = "RAMYour device is now " + state
+def notify_AWS(switches):
+	msg = {}
+	for s in switches:
+		msg[s[ID]] = s[STATE]
+	data = json.dumps(msg)
+	conn = httplib.HTTPConnection("52.39.99.178", HUB_PORT_NUMBER)
 	conn.request("POST", "", msg)
 
 
@@ -38,20 +42,67 @@ def powering_on(history):
 	old, med, new = history
 	return old < med and med < new
 
-last_state = "off"
-last_powers = [0, 0, 0]
 
-while True:
-	power = switch.current_power
-	last_powers.pop(0)
-	last_powers.append(power)
-	print "power is " + str(power)
-	if power < THRESH and powering_off(last_powers) and last_state == "on":
-		notify_AWS(OFF)
-		last_state = "off"
-	elif power < THRESH and powering_on(last_powers) and last_state == "off":
-		notify_AWS(ON)
-		last_state = "on"
-	time.sleep(1)
+def run_local_server(switches):
+	#send initial state info to AWS
+	notify_AWS(switches)
+
+	num_switches = len(switches)
+	print str(num_switches) + " Found!"
+
+	last_powers_list = [[0, 0, 0]]*num_switches
+	last_states_list = [OFF]*num_switches
+	#check power statuses forever	
+	while True:
+		change = False
+		for s, last_state, last_powers in zip(switches, last_states_list, last_powers_list):
+			power = s[SWITCH].current_power
+			last_powers.pop(0)
+			last_powers.append(power)
+			print "Switch " + s[NAME] + "has current power = " + str(power)
+			if power < THRESH and powering_off(last_powers) and last_state == ON:
+				change = True
+				last_state = OFF
+			elif power < THRESH and powering_on(last_powers) and last_state == OFF:
+				change = True
+				last_state = ON
+		#if any state has changed, notify AWS
+		if change:
+			change = False
+			notify_AWS(switches)
+		#wait for next power readout to be available
+		time.sleep(1)
 
 
+if __name__ == "__main__":
+	print "Powering up server..."
+	switches = []
+	env = Environment(on_switch, on_motion)
+	env.start()
+	#search until a switch is found
+	tries = 0
+	print "Searching for switches..."
+	while True:
+		tries += 1
+		names = env.list()
+		if len(names) > 0: break
+		if tries == 100: 
+			print "FAILED to find a switch after 100 tries"
+			sys.exit()
+	#create a dictionary object wrapper for each found switch
+	for switch_name in names:
+		s = {NAME : switch_name, SWITCH : None, STATE : OFF, ID : None, THRESH : None}
+		if switch_name == "ymcmb":
+			ymcmb = env.get_switch('ymcmb')
+			s[SWITCH] = ymcmb
+			s[ID] = YMCMB_ID
+			s[THRESH] = YMCMB_THRESH
+		elif switch_name == "paddy":
+			paddy = env.get_switch('paddy')
+			s[SWITCH] = paddy
+			s[ID] = PADDY_ID
+			s[THRESH] = PADDY_THRESH
+		#create a list of all found switches
+		switches.append(s)
+
+	run_local_server(switches)
