9e5a9f419cf569877b138bd04048a7d20e2fdef0
Ambika <aacharya@stanford.edu>
2016-06-05 22:00:59 -0700

    added unfollow

--- AWS/user_processor.py
+++ AWS/user_processor.py
@@ -115,7 +115,49 @@ def process_follow(cmd_dic, platform, conn, cursor):
     return rsp_dic
 
 def process_unfollow(cmd_dic, platform, conn, cursor):
-    pass
+    response_dic = {}
+    args = cmd_dic[c.ARGS]
+    #This command must have at least 1 argument
+    if len(args) == 0:
+        response_dic = None
+    else:
+        switch = args[0]
+        user = cmd_dic[c.USER]
+        check = "select * from switches where switch_id == " + switch + ";"
+        a = cursor.execute(check)
+        data = a.fetchall()
+        #This is a valid switch id
+        if len(data) > 0:
+            response_dic[c.SWITCH_ID] = switch
+            response_dic[c.CMD] = cmd_dic[c.CMD]
+            unique = "select * from map where switch_id == " + switch + " and user_id == " + user + " and fetty_flag == 1 and sub_timeout != null;"
+            results = cursor.execute(unique).fetchall()
+            #There is a map from switch to user with an on flag
+            if len(results) > 0:
+                cursor.execute("update map set fetty_flag = 0, sub_timeout = null where switch_id == "+switch+" and user_id == "+user+";")
+                conn.commit()
+                response_dic[c.RSP] = c.UNFOLLOW_MSGS[1]
+            # there is no (switch, id) pair in map being followed
+            else:
+                unique = "select * from map where switch_id == " + switch + " and user_id == " + user  + ";"
+                results = cursor.execute(unique).fetchall()
+                #There is a switch to user pair in map, need to follow it before they can unfollow it
+                if len(results) > 0:
+                    cursor.execute("update map set fetty_flag = 0, sub_timeout = null where switch_id == "+switch+" and user_id == "+user+";")
+                    conn.commit()
+                    response_dic[c.RSP] = c.UNFOLLOW_MSGS[1]
+                #so tell them to add it before they can unfollow it
+                else:
+                    response_dic[c.RSP] = c.ADD_MSGS[2]
+            # there is no (switch, id) pair in map, so tell them to follow it before they can unfollow it
+            else:
+                response_dic[c.RSP] = c.UNFOLLOW_MSGS[0]
+        #The supplied switch id is not a valid switch id
+        else:
+            response_dic = None
+    cursor.close()
+    conn.close()
+    return response_dic
 
 def process_add(cmd_dic, platform, conn, cursor):
     response_dic = {}
