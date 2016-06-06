8552220652dc2863a53cdb611afaaf07031f2ceb
root <root@ip-172-31-44-68.us-west-2.compute.internal>
2016-06-06 09:00:21 +0000

    everything looks good! just need to fix up follow

--- AWS/switch_processor.py
+++ AWS/switch_processor.py
@@ -14,17 +14,17 @@ from time_utils import timestamp_to_seconds
 have changed state or any user's whose subscribtion has ended"""
 def update_subscriptions(conn,cursor):
     current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-    subs_check = "select user_id, switch_id, sub_timeout from map where sub_timeout != null;"
+    subs_check = "select user_id, switch_id, sub_timeout from map where sub_timeout is not null;"
     subs = cursor.execute(subs_check)
     for sub in subs:
-        user = sub[0]
-        switch = sub[1]
+        user = str(sub[0])
+        switch = str(sub[1])
         end_time = sub[2]
         #check datatime
         end = timestamp_to_seconds(end_time)
         now = timestamp_to_seconds(current_time)
         if now > end:
-            update_map = 'update map set fetty_flag = 0, sub_timeout = null where switch_id == ' + switch_id +" and user_id == " + u_id  + ';'
+            update_map = 'update map set fetty_flag = 0, sub_timeout = null where switch_id == ' + switch +" and user_id == " + user  + ';'
             cursor.execute(update_map)
             conn.commit()
 
@@ -42,7 +42,7 @@ def notify_user(u_id,medium,state, switch_id):
 	    msg += " is now on"
 	else:
 	    msg += " is now off"
-	if (medium == c.TEXT_FLAG):
+	if (int(medium) == c.TEXT_FLAG):
 	    messager.send_message(u_id, msg)
 
 def process_switch(switch_id,state, conn, cursor):
@@ -55,7 +55,7 @@ def process_switch(switch_id,state, conn, cursor):
     if len(data) > 0:
     	state_check = "select state from switches where switch_id == " + switch_id + ";"
         a = cursor.execute(state_check)
-        cur_state = a.fetchall()[0]
+        cur_state = a.fetchall()[0][0]
         #This is a valid switch id
         if cur_state != state:
             # get all users following that switch
@@ -63,15 +63,14 @@ def process_switch(switch_id,state, conn, cursor):
             users = cursor.execute(users_check).fetchall()
             for u in users:
                 u_id = str(u[1])
-                print u_id
                 tmstp = u[3]
-                medium_check = "select user_medium from users where user_id == " + u_id + ";" 
+                medium_check = "select platform_id from users where user_id == " + u_id + ";" 
                 medium = cursor.execute(medium_check).fetchall()[0][0]
                 notify_user(u_id,medium,state, switch_id)
                 if (tmstp == None):
                     update_map = 'update map set fetty_flag = 0 where switch_id == ' + switch_id +" and user_id == " + u_id + ';'
-                cursor.execute(update_map)
-                conn.commit
+                    cursor.execute(update_map)
+                    conn.commit
 
         update = 'update switches set state = '+state+' where switch_id == ' + switch_id + ';'
         cursor.execute(update)
@@ -82,13 +81,11 @@ def process_switch(switch_id,state, conn, cursor):
 
 class UserHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(s):
-        print "It's heads"
         s.send_response(200)
         pass
 
     #handle get request
     def do_GET(s):
-        print "It's get"
         s.send_response(200)
         pass
 
@@ -99,13 +96,12 @@ class UserHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         update_subscriptions(conn,cursor)
         http_in = s.rfile.read(int(s.headers.getheader('Content-Length'))) #json
         hub_dic = json.loads(http_in)
-        print hub_dic
         for switch_id in hub_dic:
             process_switch(switch_id, hub_dic[switch_id], conn, cursor)
         cursor.close()
         conn.close()
 
 if __name__ == "__main__":
-    #sys.stderr = open("otherlog.txt", "a+")
+    sys.stderr = open("otherlog.txt", "a+")
     accept_command()
 
