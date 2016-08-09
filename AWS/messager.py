389af332880a27ee3397d9011df8fd120317f1da
root <root@ip-172-31-44-68.us-west-2.compute.internal>
2016-08-09 02:52:35 +0000

    Revert "Revert "nothing""
    This reverts commit be036ef80100024cb1544705f1fd59bc0b3aaead.

--- AWS/messager.py
+++ AWS/messager.py
@@ -3,6 +3,5 @@ import secret_tokens as tokens
 
 
 def send_message(num="+16464630213", msg="msg not specified"):
-    # put your own credentials here 
     client = TwilioRestClient(tokens.ACCOUNT_SID, tokens.AUTH_TOKEN) 
     client.messages.create(to=num, from_="+16508351609", body=msg) 
--- local/switch_maintenance.py
+++
@@ -1,7 +0,0 @@
-#	This file ensures that all the switches
-#	that this local server has access to
-#	have an accurate current avg of what it means
-#	for them to be on or off
-from ouimeaux.environment import Environment
-
-#	implement once hole-punching works #
